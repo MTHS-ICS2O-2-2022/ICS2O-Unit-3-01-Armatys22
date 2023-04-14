f382b7fd926d8e09bf2960f6e9faa17804acbfac
Armatys22 <124389724+Armatys22@users.noreply.github.com>
2023-04-14 17:03:47 +0000

    juy

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: dom
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates volume of a sphere.
+ */
+function calculate() {
+  // input
+  const length = parseInt(document.getElementById("A-base").value)
+  const width = parseInt(document.getElementById("B-base").value)
+    const height = parseInt(document.getElementById("height-of-triangle").value)
+
+  // process
+  const area = [ (length+width) / 2 ]*height
+
+
+  // output
+  document.getElementById("area").innerHTML = "Celsius is: " + area.toFixed(2) + "cm."
+}
